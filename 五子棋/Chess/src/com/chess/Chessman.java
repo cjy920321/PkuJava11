2cec6933f0b2446f10952784f3870979c8d55eac
shuxin <2425085643@qq.com>
2015-11-23 00:05:03 +0800

    五子棋

---
+++ 五子棋/Chess/src/com/chess/Chessman.java
@@ -0,0 +1,19 @@
+package com.chess; 
+public enum Chessman {
+	BLACK("●"), WHITE("○");
+	private String chessman;
+
+	/**
+	 * 私有构造器
+	 */
+	private Chessman(String chessman) {
+		this.chessman = chessman;
+	}
+
+	/**
+	 * @return String 黑棋或者白棋
+	 */
+	public String getChessman() {
+		return this.chessman;
+	}
+}
